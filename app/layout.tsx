8feda149a568167e69307a7ed70997567072cc8d
seunghyun lee <lee_tmdgus@naver.com>
2025-06-18 13:37:11 +0900

    fix: use relative paths instead of alias to resolve Vercel build issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
-import AuthNavigator from "@/components/Auth/AuthNavigator";
-import ClientLayout from "@/components/layout/ClientLayout";
+import AuthNavigator from "../components/Auth/AuthNavigator";
+import ClientLayout from "../components/layout/ClientLayout";
 
 
 import "./globals.css";
